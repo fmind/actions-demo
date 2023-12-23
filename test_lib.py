eaab74d372541ef50106e587fad71ff58b85ee42
Médéric Hurier (Fmind) <github@fmind.dev>
2023-12-23 09:47:21 +0100

    Add bad test

--- test_lib.py
+++ test_lib.py
@@ -1,10 +1,18 @@
 import lib
 
 
-def test_inc():
+def test_good_inc():
     # given
     x = 3
     # when
     res = lib.inc(x)
     # then
     assert res == 4
+
+def test_bad_inc():
+    # given
+    x = 3
+    # when
+    res = lib.inc(x)
+    # then
+    assert res == 5
